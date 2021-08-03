922f20378d5289bf008537f35a7c7307fb961b80
s-hmantha <71147291+s-hmantha@users.noreply.github.com>
2021-08-02 17:55:27 -0700

    Finished Constructor
    - added motor, pid, & feedforward values to constants
    - fleshed out constructor for flywheel class
    - wrote abstract method for profiledPiIDController class

--- 2021-offseason/src/main/java/frc/robot/Constants.java
+++ 2021-offseason/src/main/java/frc/robot/Constants.java
@@ -4,12 +4,43 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMaxLowLevel;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class kFlywheel {
+       
+        // motors
+        public static final int MAIN_ID = 0;
+        public static final int FOLLOWER_ID = 0;
+        public static final CANSparkMaxLowLevel.MotorType MOTOR_TYPE = CANSparkMaxLowLevel.MotorType.kBrushless;
+        public static final boolean MAIN_INVERTED = false;
+        public static final boolean FOLLOWER_INVERTED = false;  
+        public static final int CURRENT_LIMIT = 0;
+
+        // pid constants
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;  
+        public static final double MAX_VELOCITY = 0;
+        public static final double MAX_ACCELERATION = 0;
+        public static final int ERR_TOLERANCE = 0;
+
+        // feedforward constants
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+        
+    }
+
+}
