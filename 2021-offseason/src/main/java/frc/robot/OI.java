ec2e1096a159a139e2b09a1b7b954d31deba6b36
s-hmantha <mantha.hitesh@gmail.com>
2021-08-09 16:25:27 -0700

    extra stuff I forgot to commit

--- 2021-offseason/src/main/java/frc/robot/OI.java
+++ 2021-offseason/src/main/java/frc/robot/OI.java
@@ -1,3 +1,5 @@
+package frc.robot;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID;
 
--- 2021-offseason/src/main/java/frc/robot/RobotContainer.java
+++ 2021-offseason/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,9 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Flywheel;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -22,6 +25,7 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Flywheel m_flywheel = new Flywheel();
 
   private final Hopper m_hopper = new Hopper();
 
