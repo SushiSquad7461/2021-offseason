04395f13d6fb01511eb1a85c74c3d20021c1c2ce
Aadit <aadit.ambadkar@gmail.com>
2021-08-09 18:45:21 -0700

    Finished Most of Hood

--- 2021-offseason/src/main/java/frc/robot/Constants.java
+++ 2021-offseason/src/main/java/frc/robot/Constants.java
@@ -4,12 +4,54 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.team254.lib.util.InterpolatingDouble;
+import com.team254.lib.util.InterpolatingTreeMap;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class Hood {
+        public static final int MOTOR_ID = 12;
+        public static final CANSparkMaxLowLevel.MotorType MOTOR_TYPE = CANSparkMaxLowLevel.MotorType.kBrushless;
+        public static final double INITIAL_SETPOINT = 80.0;
+        public static final double MAX_SPEED = 0.3;
+        public static final double kP = 0.1;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double SETPOINT = (45.0/360.0) * (332.0/14.0) * 15;
+        public static final InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> hoodAngleTreeMap = new InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble>();
+        public static final double[] ZONE_SETPOINTS = {37.0, 56.0, 80.0, 80.0};
+        public static final int LIMIT_PORT = -1;
+
+        static {
+            hoodAngleTreeMap.put(new InterpolatingDouble(50.0), new InterpolatingDouble(39.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(60.0), new InterpolatingDouble(41.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(70.0), new InterpolatingDouble(45.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(80.0), new InterpolatingDouble(49.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(90.0), new InterpolatingDouble(52.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(100.0), new InterpolatingDouble(54.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(110.0), new InterpolatingDouble(56.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(120.0), new InterpolatingDouble(57.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(130.0), new InterpolatingDouble(58.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(140.0), new InterpolatingDouble(59.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(150.0), new InterpolatingDouble(62.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(160.0), new InterpolatingDouble(64.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(170.0), new InterpolatingDouble(65.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(180.0), new InterpolatingDouble(65.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(190.0), new InterpolatingDouble(65.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(200.0), new InterpolatingDouble(66.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(210.0), new InterpolatingDouble(65.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(220.0), new InterpolatingDouble(66.0));
+            hoodAngleTreeMap.put(new InterpolatingDouble(230.0), new InterpolatingDouble(67.0));
+        }
+    }
+}
---
+++ 2021-offseason/src/main/java/frc/robot/commands/Hood.java
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.team254.lib.util.InterpolatingDouble;
+import com.team254.lib.util.InterpolatingTreeMap;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.ControlType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import org.photonvision.*;
+
+public class Hood extends SubsystemBase {
+    private CANSparkMax hoodMain;
+    private CANEncoder hoodEncoder;
+    private CANPIDController hoodController;
+    private InterpolatingTreeMap closeTreeMap;
+    private InterpolatingTreeMap farTreeMap;
+    private PhotonCamera camera;
+    private double curdeg = 0;
+    private DigitalInput limitswitch = new DigitalInput(Constants.Hood.LIMIT_PORT);
+    public Hood() {
+    
+        SmartDashboard.putNumber("kP", Constants.Hood.kP);
+        SmartDashboard.putNumber("kD", Constants.Hood.kD);
+        this.hoodMain = new CANSparkMax(Constants.Hood.MOTOR_ID, Constants.Hood.MOTOR_TYPE);
+        hoodMain.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        this.hoodEncoder = this.hoodMain.getEncoder();
+        this.hoodController = this.hoodMain.getPIDController();
+        this.hoodController.setOutputRange(-Constants.Hood.MAX_SPEED, Constants.Hood.MAX_SPEED);
+        this.hoodController.setP(Constants.Hood.kP);
+        this.hoodController.setI(Constants.Hood.kI);
+        this.hoodController.setD(Constants.Hood.kD);
+        this.zeroHood();
+        this.camera = new PhotonCamera("myCamera");
+        //this.setSetpoint(50);
+        
+    }
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("real output", hoodMain.getAppliedOutput());
+        SmartDashboard.putNumber("bruh position", hoodEncoder.getPosition());
+    }
+
+    public void zeroHood() {
+        if (limitswitch.get()) hoodMain.set(0);
+        else hoodMain.set(0.7);
+    }
+
+    private void setSetpoint(double setpoint) {
+        this.hoodController.setReference(setpoint, ControlType.kPosition);
+    }
+
+    public void increaseSetpoint(double amount) {
+        curdeg+=amount;
+        curdeg=Math.max(0, curdeg);
+        setSetpoint(curdeg+Constants.Hood.INITIAL_SETPOINT);
+    }
+    
+}
