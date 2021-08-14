262a87a1e656c093534a8cd96bda24a0d2c4e329
Aadit Ambadkar <58674441+Aadit-Ambadkar@users.noreply.github.com>
2021-08-14 14:58:23 -0700

    Update Constants.java

--- 2021-offseason/src/main/java/frc/robot/Constants.java
+++ 2021-offseason/src/main/java/frc/robot/Constants.java
@@ -8,16 +8,6 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import com.team254.lib.util.InterpolatingDouble;
 import com.team254.lib.util.InterpolatingTreeMap;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This class should not be used for any other
- * purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
- */
 public final class Constants {
     public static final class Hood {
         public static final int MOTOR_ID = 12;
