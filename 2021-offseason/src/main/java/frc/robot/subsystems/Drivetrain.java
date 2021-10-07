ce7b94003531f11fa81ddf971c53066588d8a9fa
Aadit <aadit.ambadkar@gmail.com>
2021-10-06 17:52:18 -0700

    Push Because Jared Said So

--- 2021-offseason/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2021-offseason/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,11 +9,15 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+
 public class Drivetrain extends SubsystemBase {
   private final CANSparkMax frontLeft, frontRight, backLeft, backRight;
   private final DifferentialDrive diffDrive;
   private int angleInvert;
   private boolean slow;
+  private PhotonCamera camera;
 
   public Drivetrain() {
     frontLeft = new CANSparkMax(Constants.kDrivetrain.FRONT_LEFT_ID, Constants.kDrivetrain.MOTOR_TYPE);
@@ -27,7 +31,7 @@ public class Drivetrain extends SubsystemBase {
     backRight.restoreFactoryDefaults();
 
     diffDrive = new DifferentialDrive(frontLeft, frontRight);
-    //front motors are controlled, others follow corresponding
+    // front motors are controlled, others follow corresponding
     backLeft.follow(frontLeft);
     backRight.follow(frontRight);
 
@@ -48,6 +52,7 @@ public class Drivetrain extends SubsystemBase {
     frontRight.setSmartCurrentLimit(Constants.kDrivetrain.CURRENT_LIMIT);
     backLeft.setSmartCurrentLimit(Constants.kDrivetrain.CURRENT_LIMIT);
     backRight.setSmartCurrentLimit(Constants.kDrivetrain.CURRENT_LIMIT);
+    this.camera = new PhotonCamera("myCamera");
   }
 
   public void curveDrive(double linearVelocity, double angularVelocity, boolean isQuickturn) {
@@ -56,7 +61,8 @@ public class Drivetrain extends SubsystemBase {
     }
 
     if (slow) {
-      diffDrive.curvatureDrive(linearVelocity * Constants.kDrivetrain.SLOW_SPEED, angularVelocity * angleInvert, isQuickturn);
+      diffDrive.curvatureDrive(linearVelocity * Constants.kDrivetrain.SLOW_SPEED, angularVelocity * angleInvert,
+          isQuickturn);
     } else {
       diffDrive.curvatureDrive(linearVelocity, angularVelocity * angleInvert, isQuickturn);
     }
@@ -76,6 +82,13 @@ public class Drivetrain extends SubsystemBase {
 
   public void endSlow() {
     slow = false;
+  }
+
+  public void alignToTarget() {
+    var result = camera.getLatestResult();
+    if (result.hasTargets()) {
+      double yaw = result.getBestTarget().getYaw();
+    }
   }
 
   @Override
